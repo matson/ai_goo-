c3c7ff926e0be8ce49a6070fbed2b6d7a9972903
Madison Adams <madison.k.adams@gmail.com>
2025-10-07 22:06:19 -0300

    dataset file

---
+++ tensor-simple-dataset.py
@@ -0,0 +1,34 @@
+
+
+# creating a dataset class in tensor 
+# this will serve as a template for future use 
+
+# imports 
+import torch
+from torch.utils.data import Dataset
+
+# define class for dataset
+class toy_set(Dataset):
+    
+    # Constructor with defult values 
+    def __init__(self, length = 100, transform = None):
+        self.len = length
+        self.x = 2 * torch.ones(length, 2)
+        self.y = torch.ones(length, 1)
+        self.transform = transform
+     
+    # Getter
+    def __getitem__(self, index):
+        sample = self.x[index], self.y[index]
+        if self.transform:
+            sample = self.transform(sample)     
+        return sample
+    
+    # Get Length
+    def __len__(self):
+        return self.len
+    
+our_dataset = toy_set()
+print("Our toy_set object: ", our_dataset)
+print("Value on index 0 of our toy_set object: ", our_dataset[0])
+print("Our toy_set length: ", len(our_dataset))
